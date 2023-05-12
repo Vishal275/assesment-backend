5c4102f7469febf3b0bfc5b75277aa8be33da34a
vishalkudtarkar <vishal.kudtarkar@techsierra.in>
2023-05-12 20:09:36 +0530

    catch

--- api/users.js
+++ api/users.js
@@ -48,7 +48,7 @@ router.get("/all-users", async (req, res) => {
 
 router.post("/edit-score", async (req, res) => {
   try {
-    const {user_id, score} = req.body
+    const {user_id, score} = req.body;
     User.findOne({_id: user_id}).then((user) => {
       if(!user) {
         return res.status(400).json({
@@ -73,6 +73,12 @@ router.post("/edit-score", async (req, res) => {
         }
         return res.status(201).json({
           message: "Success",
+        });
+      }).catch((error) => {
+        console.log(`Error while entering data : ${error}`);
+        return res.status(500).json({
+          message:
+            "There was some problem processing the request. Please try again later.",
         });
       }).catch((error) => {
         console.log(`Error while entering data : ${error}`);
